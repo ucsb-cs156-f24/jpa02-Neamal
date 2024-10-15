24708572dd637d9d30e6e3c088234c624d9ce49d
Junhyung Yoon <junhyungyoon@169-231-125-108.wireless.ucsb.edu>
2024-10-14 23:02:08 -0700

    edit

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,6 +19,70 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void first2_equals_test() {
+        Team team = new Team("test-team");
+        assertEquals(team.equals(team), true);
+        assertEquals(team.equals("wrong"), false);
+    }
+
+    @Test
+    public void true_true_equals_test() {
+        Team t1 = new Team("correct");
+        Team t2 = new Team("correct");
+        t1.addMember("correct");
+        t2.addMember("correct");
+        assertEquals(t1.equals(t2), true);
+    }
+
+    @Test
+    public void true_false_equals_test() {
+        Team t1 = new Team("correct");
+        Team t2 = new Team("correct");
+        t1.addMember("correct");
+        t2.addMember("wrong");
+        assertEquals(t1.equals(t2), false);
+    }
+
+    @Test
+    public void false_true_equals_test() {
+        Team t1 = new Team("correct");
+        Team t2 = new Team("wrong");
+        t1.addMember("correct");
+        t2.addMember("correct");
+        assertEquals(t1.equals(t2), false);
+    }
+
+    @Test
+    public void false_false_equals_test() {
+        Team t1 = new Team("correct");
+        Team t2 = new Team("wrong");
+        t1.addMember("correct");
+        t2.addMember("wrong");
+        assertEquals(t1.equals(t2), false);
+    }
+
+    @Test
+    public void hash_code_test() {
+        Team t1 = new Team("jun");
+        t1.addMember("yoon");
+        Team t2 = new Team("jun");
+        t2.addMember("yoon");
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hash_code_test_2() {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
